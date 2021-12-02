87e76df15817f94576b91e24a28a3af11fa6f44b
Jacob Langley <jacob.langley@ghtns.com>
2021-12-01 21:24:06 -0800

    Add d2p1

---
+++ day2/p1/script.py
@@ -0,0 +1,40 @@
+#!python3
+from itertools import islice
+import logging
+
+logging.basicConfig(filemode='w', level=logging.DEBUG)
+
+WINDOWSIZE = 3
+
+def slurpFile(path):
+	"""Retrieve file contents"""
+	contents = list()
+	with open(path, 'r') as f:
+		for l in f:
+			contents.append(l.rstrip())
+	return contents
+
+def parse(line):
+	""" """
+	(command, value) = line.split(' ')
+	return(command, int(value))
+
+def main():
+	contents = list(map(parse, slurpFile("input")))
+
+	horiz = 0
+	depth = 0
+	
+	for c in contents:
+		if(c[0] == "forward"):
+			horiz += c[1]
+		elif(c[0] == "down"):
+			depth += c[1]
+		elif(c[0] == "up"):
+			depth -= c[1]
+
+	print("Horizontal Postition: " + str(horiz))
+	print("Depth: " + str(depth))
+	print("H*D: " + str(horiz * depth))
+
+main()
