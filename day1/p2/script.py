461d8723c4174659667c3a101341f0284149e0ac
Jacob Langley <jacob.langley@ghtns.com>
2021-12-01 21:11:16 -0800

    Rewrite d1p2

--- day1/p2/script.py
+++ day1/p2/script.py
@@ -1,28 +1,46 @@
 #!python3
-import collections
+from itertools import islice
 import logging
 
 logging.basicConfig(filename='script.log', filemode='w', level=logging.DEBUG)
 
 WINDOWSIZE = 3
 
-increase = 0
-prev = -1
-window = collections.deque(maxlen=WINDOWSIZE)
+def slurpFile(path):
+	"""Retrieve file contents"""
+	contents = list()
+	with open("input", 'r') as f:
+		for l in f:
+			contents.append(l.rstrip())
+	return contents
 
-with open("input", 'r') as f:
-	for l in f:
-		i = int(l)
-		window.append(i)
-		if(len(window) < WINDOWSIZE):
-			continue
-		logging.debug(repr(window))
-		windowSize = sum(window)
-		if windowSize > prev:
-			if prev != -1 and len(window) == WINDOWSIZE:
-				increase+=1
+# window() definition taken from Python docs https://docs.python.org/release/2.3.5/lib/itertools-example.html
+def window(seq, n=3):
+    "Returns a sliding window (of width n) over data from the iterable"
+    "   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   "
+    it = iter(seq)
+    result = tuple(islice(it, n))
+    if len(result) == n:
+        yield result
+    for elem in it:
+        result = result[1:] + (elem,)
+        yield result
+
+def main():
+	increase = 0
+	prev = -1
+
+	contents = list(map(int, slurpFile("input")))
+
+	for w in window(contents, n=WINDOWSIZE):
+		logging.debug(repr(w))
+		windowSize = sum(w)
+		if prev == -1:
+			pass #do nothing.  Its the first iteration.
+		elif windowSize > prev:
+			increase+=1
 		prev = windowSize
 
-	f.close()
+	print(increase)
 
-print(increase)
+main()
