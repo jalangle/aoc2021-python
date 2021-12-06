a00577c9cb539a06a85a50d9a61f87cbb0c2a975
Jacob Langley <jacob.langley@ghtns.com>
2021-12-06 12:12:58 -0800

    d6p1 solution

--- day6/script1.py
+++ day6/script1.py
@@ -4,17 +4,31 @@ import logging
 
 logging.basicConfig(filemode='w', level=logging.DEBUG)
 
+REPRODUCTION_RATE = 7
+REPRODUCTION_MATURITY_RATE = 2
+
+NUMBER_OF_DAYS = 80
+
 def parseFile(path):
 	"""Retrieve file contents"""
 	contents = None
 	with open(path, 'r') as f:
 		for l in f:
 			l = l.rstrip()
-			contents = l.split(",")
+			contents = [int(x) for x in l.split(",")]
 	return contents
 
 def main():
-	contents = parseFile("test")
-	print(contents)
+	lanternfishes = parseFile("input")
 
+	for day in range(0,NUMBER_OF_DAYS):
+		newToAdd = 0
+		for i in range(0, len(lanternfishes)):
+			lanternfishes[i] -= 1
+			if(lanternfishes[i] < 0):
+				lanternfishes[i] = REPRODUCTION_RATE - 1
+				lanternfishes.append(REPRODUCTION_MATURITY_RATE + REPRODUCTION_RATE -1)
+#		print("After " + str(day) + " days: " + str(lanternfishes))
+
+	print("Count: " + str(len(lanternfishes)))
 main()
