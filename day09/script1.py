3fedca7824d3fd58709d065b616f87471d36f67c
Jacob Langley <jacob.langley@ghtns.com>
2021-12-08 22:16:20 -0800

    day 9 solutions

--- day09/script1.py
+++ day09/script1.py
@@ -3,13 +3,67 @@
 import logging
 logging.basicConfig(filemode='w', level=logging.DEBUG)
 
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 def parseFile(path):
-	return "Hello, World"
+	heightmap = list()
+	with open(path, 'r') as f:
+		for l in f:
+			row = list()
+			l = l.rstrip()
+			for c in l:
+				row.append(int(c))
+			heightmap.append(row)
+		f.close()
+	return heightmap
+
+def get_north(data, x, y):
+	count_rows = len(data)
+	if( x == 0 ): #can't go north so return something larger than it can be
+		return 1000000
+	return data[x-1][y]
+
+def get_south(data, x, y):
+	count_rows = len(data)
+	if( x == count_rows - 1): #can't go south so return something larger than it can be
+		return 1000000
+	return data[x+1][y]
+
+def get_east(data, x, y):
+	count_cols = len(data[0])
+	if( y == count_cols -1 ): #can't go east so return something larger than it can be
+		return 1000000
+	return data[x][y+1]
+
+def get_west(data, x, y):
+	count_cols = len(data[0])
+	if( y == 0): #can't go east so return something larger than it can be
+		return 1000000
+	return data[x][y-1]
+
 
 def main():
-	contents = parseFile("test")
-	print(contents)
+	heightmap = parseFile("input")
+#	pp.pprint(heightmap)
+	count_rows = len(heightmap)
+	count_cols = len(heightmap[0])
+
+	risklevel = 0
+	for i in range(0, count_rows):
+		for j in range(0, count_cols):
+			val = heightmap[i][j]
+			n = get_north(heightmap, i, j)
+			s = get_south(heightmap, i, j)
+			e = get_east(heightmap, i, j)
+			w = get_west(heightmap, i, j)
+
+			if val < n and val < s and val < e and val < w:
+#				print("(" + str(i) + "," + str(j) + ")")
+				risklevel += (1 + val)
+
+
+	print(risklevel)
 	return
 
 main()
