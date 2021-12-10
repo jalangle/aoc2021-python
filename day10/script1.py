1686bff260b4d4966dc2c99e16170a5db19a293e
Jacob Langley <jacob.langley@ghtns.com>
2021-12-10 06:28:41 -0800

    day10 solutions

--- day10/script1.py
+++ day10/script1.py
@@ -3,13 +3,76 @@
 import logging
 logging.basicConfig(filemode='w', level=logging.DEBUG)
 
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+from collections import deque
+
+validOpens = ["(", "[", "{", "<"]
+validCloses = [")", "]", "}", ">"]
+closeToOpen = { ")" : "(", "]" : "[", "}" : "{", ">" : "<" }
+
+closePoints = {
+	")" : 3,
+	"]" : 57,
+	"}" : 1197,
+	">" : 25137,
+}
 
 def parseFile(path):
-	return "Hello, World"
+	contents = list()
+	with open(path, 'r') as f:
+		for l in f:
+			contents.append(l.rstrip())
+	return contents
+
+def validate(chunk):
+
+	stack = deque()
+	for c in chunk:
+		if c in validOpens:
+#			print("open " + c)
+			stack.append(c)
+		elif c in validCloses:
+			lastChar = None
+			if(len(stack) > 0):
+				lastChar = stack.pop()
+#			print("close " + c + ", " + lastChar)
+			if(closeToOpen[c] != lastChar):
+				return (False, c)
+		else:
+#			print( "Wow I screwed up")
+			return (False, None)
+
+	return (True, None)
 
 def main():
-	contents = parseFile("test")
-	print(contents)
+	contents = parseFile("input")
+
+	invalidCharCounts = {
+		")" : 0,
+		"]" : 0,
+		"}" : 0,
+		">" : 0,	
+	}
+	for chunk in contents:
+		(valid, corruptChar) = validate(chunk)
+		if valid:
+			# this is valid to the end but missing characters
+			# print(chunk)
+			pass
+		else:
+			#print("corrupt " + corruptChar)
+			invalidCharCounts[corruptChar] += 1
+			# this is the corrupt case
+			pass
+
+	score = 0
+	for x in invalidCharCounts:
+		count = invalidCharCounts[x]
+		pointValue = closePoints[x]
+		score += (count * pointValue)
+	print(score)
 	return
 
 main()
--- day10/script2.py
+++ day10/script2.py
@@ -3,13 +3,101 @@
 import logging
 logging.basicConfig(filemode='w', level=logging.DEBUG)
 
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+from collections import deque
+
+validOpens = ["(", "[", "{", "<"]
+validCloses = [")", "]", "}", ">"]
+openToClose = { 
+	"(" : ")",
+	"[": "]",
+	"{" : "}",
+	"<" : ">"
+}
+closeToOpen = {
+	")" : "(",
+	"]" : "[",
+	"}" : "{",
+	">" : "<"
+}
+closePoints = {
+	")" : 1,
+	"]" : 2,
+	"}" : 3,
+	">" : 4,
+}
 
 def parseFile(path):
-	return "Hello, World"
+	contents = list()
+	with open(path, 'r') as f:
+		for l in f:
+			contents.append(l.rstrip())
+	return contents
+
+def validate(chunk):
+
+	stack = deque()
+	for c in chunk:
+		if c in validOpens:
+#			print("open " + c)
+			stack.append(c)
+		elif c in validCloses:
+			lastChar = None
+			if(len(stack) > 0):
+				lastChar = stack.pop()
+#			print("close " + c + ", " + lastChar)
+			if(closeToOpen[c] != lastChar):
+				return (False, c)
+		else:
+#			print( "Wow I screwed up")
+			return (False, None)
+
+	completion = ""
+	while(len(stack) > 0):
+		completion += openToClose[stack.pop()]
+
+	return (True, completion)
+
+def scoreCompletion(completion):
+	s = 0
+	for c in completion:
+		s *= 5
+		s += closePoints[c]
+
+	return s;
 
 def main():
-	contents = parseFile("test")
-	print(contents)
-	return
+	contents = parseFile("input")
+
+	invalidCharCounts = {
+		")" : 0,
+		"]" : 0,
+		"}" : 0,
+		">" : 0,	
+	}
+	scores = list()
+
+	for chunk in contents:
+		(valid, extra) = validate(chunk)
+		if valid:
+			# this is valid to the end but missing characters
+#			print(chunk + "    " + extra)
+			s = scoreCompletion(extra)
+#			print(s)
+			scores.append(s)
+			pass
+		else:
+			#print("corrupt " + extra)
+			invalidCharCounts[extra] += 1
+			# this is the corrupt case
+			pass
+
+	scores.sort()
+	pp.pprint(scores)
+	middleIndex = int((len(scores)) / 2)
+	pp.pprint(middleIndex)
+	pp.pprint(scores[middleIndex])
 
 main()
