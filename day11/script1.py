f14f10d287ed6c4789f23ddeefa6a9494bd97b0a
Jacob Langley <jacob.langley@ghtns.com>
2021-12-11 19:37:26 -0800

    day11 solutions

--- day11/script1.py
+++ day11/script1.py
@@ -10,12 +10,105 @@ def parseFile(path):
 	contents = list()
 	with open(path, 'r') as f:
 		for l in f:
-			contents.append(l.rstrip())
+			line = list()
+			for c in l.rstrip():
+				line.append(int(c))
+			contents.append(line)
 	return contents
 
+def printSep(label):
+	print("-" * 20 + " " + label + " " + "-" * 20)
+
+def increaseEnergy(octopodes):
+	for x in range(0, len(octopodes)):
+		for y in range(0, len(octopodes[0])):
+			octopodes[x][y] += 1
+	return octopodes
+
+def resetFlashed(octopodes):
+	flashed = 0
+	for x in range(0, len(octopodes)):
+		for y in range(0, len(octopodes[0])):
+			if(octopodes[x][y] > 9):
+				flashed += 1
+				octopodes[x][y] = 0
+	return flashed
+
+def increaseNeighbor(octopodes, x, y, hasFlashed):
+	if(x < 0):
+		return
+	if(x >= len(octopodes)):
+		return
+	if(y < 0):
+		return
+	if(y >= len(octopodes[0])):
+		return
+	octopodes[x][y] += 1
+	if(octopodes[x][y] > 9):
+		#print("Chain: " + strPt(x,y))
+		flashOctopode(octopodes, x, y, hasFlashed)
+		pass
+
+def strPt(x, y):
+	return "(" + str(x) + "," + str(y) + ")"
+
+def flashOctopode(octopodes, x, y, hasFlashed):
+	if (x,y) in hasFlashed:
+		return
+
+	hasFlashed.add((x,y))
+
+	# increase the NW corner
+	increaseNeighbor(octopodes, x-1, y-1, hasFlashed)
+
+	#increase the north
+	increaseNeighbor(octopodes, x-1, y, hasFlashed)
+
+	#increase the NE corner
+	increaseNeighbor(octopodes, x-1, y+1, hasFlashed)
+
+	#increase the west
+	increaseNeighbor(octopodes, x, y-1, hasFlashed)
+
+	#increase the east
+	increaseNeighbor(octopodes, x, y+1, hasFlashed)
+
+	# increase the SW corner
+	increaseNeighbor(octopodes, x+1, y-1, hasFlashed)
+
+	#increase the S
+	increaseNeighbor(octopodes, x+1, y, hasFlashed)
+
+	#increase the NE corner
+	increaseNeighbor(octopodes, x+1, y+1, hasFlashed)
+
+def flashOctopodes(octopodes):
+	hasFlashed = set()
+	for x in range(0, len(octopodes)):
+		for y in range(0, len(octopodes[0])):
+			if(octopodes[x][y] > 9 and (x,y) not in hasFlashed):
+				#print("Origin: " + strPt(x,y))
+				#print("Flashed: " + str(hasFlashed))
+				flashOctopode(octopodes, x, y, hasFlashed)
+	return len(hasFlashed)
+
 def main():
-	contents = parseFile("test")
-	print(contents)
+	octopodes = parseFile("input")
+	printSep("Initial")
+	pp.pprint(octopodes)
+
+	flashes = 0
+	days = 100
+	for step in range(0,days):
+		printSep("Day " + str(step + 1))
+		increaseEnergy(octopodes)
+		flashes += flashOctopodes(octopodes)
+
+		resetFlashed(octopodes)
+		#pp.pprint(octopodes)
+
+	print("Flashes: " + str(flashes))
+
 	return
 
 main()
