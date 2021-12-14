1f908d2a7ad2c5d5af07e75cc40f83025888801c
Jacob Langley <jacob.langley@ghtns.com>
2021-12-14 11:29:23 -0800

    day14 part 2

--- day14/script2.py
+++ day14/script2.py
@@ -1,21 +1,61 @@
 #!python3
 
 import logging
-logging.basicConfig(filemode='w', level=logging.DEBUG)
+logging.basicConfig(filemode='w', level=logging.INFO)
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
+STEPS = 40
+
 def parseFile(path):
-	contents = list()
+	initial = ""
+	insertions = dict()
 	with open(path, 'r') as f:
 		for l in f:
-			contents.append(l.rstrip())
-	return contents
+			l = l.rstrip()
+			if(initial == ""):
+				initial = l
+			elif(l != ""):
+				(pair, insertion) = l.split(" -> ")
+				insertions[pair] = insertion
+	return (initial, insertions)
 
 def main():
-	contents = parseFile("test")
-	print(contents)
+	(polymer, insertions) = parseFile("input")
+
+	segmentCounts = dict.fromkeys(insertions, 0)
+
+	for i in range(0,len(polymer)-1):
+		segment = polymer[i:i+2]
+		segmentCounts[segment] += 1
+
+	for step in range(0, STEPS):
+		counts = dict.fromkeys(segmentCounts, 0)
+		for segment in segmentCounts:
+			insertion = insertions[segment]
+			counts[segment[0] + insertion] += segmentCounts[segment]
+			counts[insertion + segment[1]] += segmentCounts[segment]
+		segmentCounts = counts
+
+	letterCounts = dict()
+	for segment in segmentCounts:
+		for c in segment:
+			if c in letterCounts:
+				letterCounts[c] += segmentCounts[segment]
+			else:
+				letterCounts[c] = segmentCounts[segment]
+
+	for letter in letterCounts:
+		if letterCounts[letter] % 2 == 1:
+			letterCounts[letter] = ( letterCounts[letter] // 2 ) + 1
+		else:
+			letterCounts[letter] = ( letterCounts[letter] // 2 )
+
+	maximum = max(x for x in letterCounts.values())
+	minimum = min(x for x in letterCounts.values())
+
+	print(str(maximum - minimum))
 	return
 
 main()
