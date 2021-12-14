293e7163a3384dbea3404e259ee16a259c234638
Jacob Langley <jacob.langley@ghtns.com>
2021-12-14 07:15:58 -0800

    day14 part 1 solution

--- day14/script1.py
+++ day14/script1.py
@@ -6,16 +6,60 @@ logging.basicConfig(filemode='w', level=logging.DEBUG)
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
+STEPS = 10
+
 def parseFile(path):
-	contents = list()
+	initial = ""
+	insertions = dict()
 	with open(path, 'r') as f:
 		for l in f:
-			contents.append(l.rstrip())
-	return contents
+			l = l.rstrip()
+			if(initial == ""):
+				initial = l
+			elif(l != ""):
+				(pair, insertion) = l.split(" -> ")
+				insertions[pair] = insertion
+	return (initial, insertions)
+
+def insertAt(strOriginal, strToInsert, pos):
+	return strOriginal[:pos] + strToInsert + strOriginal[pos:]
 
 def main():
-	contents = parseFile("test")
-	print(contents)
+	(polymer, insertions) = parseFile("input")
+
+	for s in range(0, STEPS):
+		print("STEP " + str(s))
+		for i in range(len(polymer)-2, -1, -1):
+			#print("Pos: " + str(i))
+			pair = polymer[i:i+2]
+			#print("  Pair: " + pair)
+			#print("  Before: " + polymer)
+			if(pair in insertions):
+				#print("  Insertion: " + insertions[pair])
+				polymer = insertAt(polymer, insertions[pair], i + 1)
+			#print("  After: " + polymer)
+			#print("-" * 40)
+
+	print("-" * 40)
+	counts = dict()
+	for c in polymer:
+		if c in counts:
+			counts[c] += 1
+		else:
+			counts[c] = 1
+
+	pp.pprint(counts)
+
+	maxCount = 0
+	minCount = 1000000000000000000000000
+	for c in counts:
+		if counts[c] > maxCount:
+			maxCount = counts[c]
+		elif counts[c] < minCount:
+			minCount = counts[c]
+
+	print(str(maxCount - minCount))
+
 	return
 
 main()
