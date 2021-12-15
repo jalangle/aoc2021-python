babcece946a07b9d32adeab15b7b440ce3a9392e
Jacob Langley <jacob.langley@ghtns.com>
2021-12-15 15:26:35 -0800

    add day15 solutions

--- day15/script1.py
+++ day15/script1.py
@@ -1,21 +1,81 @@
 #!python3
 
 import logging
-logging.basicConfig(filemode='w', level=logging.DEBUG)
+logging.basicConfig(filemode='w', level=logging.INFO)
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
+
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def parseFile(path):
 	contents = list()
 	with open(path, 'r') as f:
 		for l in f:
-			contents.append(l.rstrip())
+			vals = list()
+			for c in l.rstrip():
+				vals.append(int(c))
+			contents.append(vals)
 	return contents
 
+def GetCost(grid, r, c):
+	if r < 0:
+		return None
+	if r >= len(grid):
+		return None
+	if c < 0:
+		return None
+	if c >= len(grid[0]):
+		return None
+	return grid[r][c]
+	
+def GetName(r, c):
+	return str(r) + "," + str(c)
+
+def GetKey(r_from, c_from, r_to, c_to):
+	return GetName(r_from, c_from) + "-" + GetName(r_to, c_to)
+
+def AddEdge(grid, graph, costs, r_from, c_from, r_to, c_to):
+	fromName = GetName(r_from, c_from)
+	toName = GetName(r_to, c_to)
+
+	cost = GetCost(grid, r_to, c_to)
+	if(cost):
+		costs[fromName + "-" + toName] = cost
+		graph.add_edge(fromName, toName, weight = cost)	
+
 def main():
-	contents = parseFile("test")
-	print(contents)
+	grid = parseFile("input")
+#	pp.pprint(grid)
+#	print("-" * 40)
+	g = nx.DiGraph()
+
+	costs = dict()
+	for r in range(len(grid)):
+		for c in range(len(grid[0])):
+			name = GetName(r, c)
+
+			AddEdge(grid, g, costs, r, c, r-1, c)
+			AddEdge(grid, g, costs, r, c, r+1, c)
+			AddEdge(grid, g, costs, r, c, r, c-1)
+			AddEdge(grid, g, costs, r, c, r, c+1)
+
+#	print("-" * 40)
+
+	#nx.draw(g,with_labels=True)
+	#plt.show()
+	path = nx.shortest_path(g, GetName(0, 0), GetName(len(grid)-1,len(grid[0]) -1), weight='weight')
+
+	cost = 0
+	for i in range(len(path) - 1):
+		segment = path[i:i+2]
+		key = segment[0] + "-" + segment[1]
+		print(key)
+#		print(key + " => " + str(costs[key]))
+		cost += costs[key]
+
+	print(cost)
 	return
 
 main()
